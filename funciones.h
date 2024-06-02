45b464ffdd6ffda6d9e1daefc246552edb094836
Carloscacs20 <167730021+Carloscacs20@users.noreply.github.com>
2024-06-02 15:48:23 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,8 @@
+#define maxCant 100
+#define NombreMax 30
+
+void IngresarFruta(char nombres[][NombreMax], int cantidad[], float precio[], int frutas);
+void EditarFruta(char nombres[][NombreMax], int cantidad[], float precio[], int frutas);
+void EliminarFruta(char nombres[][NombreMax], int cantidad[], float precio[], int frutas);
+void ListarFrutas(char nombres[][NombreMax], int cantidad[], float precio[], int frutas);
+int buscarFruta(char nombres[][NombreMax], int frutas, char nombre[]);
